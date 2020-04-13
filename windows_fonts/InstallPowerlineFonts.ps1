0cfb7ec4cd4ee4c4db8b4b133710eca8f892f245
dev7k <dev7k@protonmail.com>
2020-04-13 12:22:50 +0200

    Added powerline fonts installation script

---
+++ windows_fonts/InstallPowerlineFonts.ps1
@@ -0,0 +1,5 @@
+Write-Host "Cloning powerline fonts repository"
+git clone https://github.com/powerline/fonts c:\develop\github\fonts
+Write-Host "Installing powerline fonts"
+c:\develop\github\fonst\install.ps1
+Write-Host "Powerline fonts installed"
