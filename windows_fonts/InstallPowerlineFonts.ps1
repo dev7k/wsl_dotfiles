af3870b45383b19f5cfe3dafcf2562365cd552b3
dev7k <dev7k@protonmail.com>
2020-04-13 12:28:48 +0200

    Fixed typo

--- windows_fonts/InstallPowerlineFonts.ps1
+++ windows_fonts/InstallPowerlineFonts.ps1
@@ -1,5 +1,5 @@
 Write-Host "Cloning powerline fonts repository"
 git clone https://github.com/powerline/fonts c:\develop\github\fonts
 Write-Host "Installing powerline fonts"
-c:\develop\github\fonst\install.ps1
+c:\develop\github\fonts\install.ps1
 Write-Host "Powerline fonts installed"
