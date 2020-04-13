f4bf6be05e1ef3029fc300de09442cb39c980428
dev7k <dev7k@protonmail.com>
2020-04-13 21:07:54 +0200

    Fixed script

--- windows_fonts/InstallPowerlineFonts.ps1
+++ windows_fonts/InstallPowerlineFonts.ps1
@@ -1,5 +1,11 @@
 Write-Host "Cloning powerline fonts repository"
-git clone https://github.com/powerline/fonts c:\develop\github\fonts
+git clone https://github.com/powerline/fonts --depth=1
+Write-Host "Go to folder fonts"
+cd fonts
 Write-Host "Installing powerline fonts"
-c:\develop\github\fonts\install.ps1
+./install.ps1
 Write-Host "Powerline fonts installed"
+Write-Host "Removing files"
+cd ..
+rm -R .\fonts
+Write-Host "Files removed"
