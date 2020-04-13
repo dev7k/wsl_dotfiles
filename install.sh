b4edb584230a4513d239d9b709050deb6e1df8f2
dev7k <dev7k@protonmail.com>
2020-04-13 11:40:56 +0200

    Added installation script

---
+++ install.sh
@@ -0,0 +1,28 @@
+# Update and upgrade packages
+sudo apt update && sudo apt upgrade
+
+# Install packages
+sudo apt install git
+sudo apt install tig
+sudo apt install tmux
+sudo apt install mc
+sudo apt install pydf
+sudo apt install tree
+sudo apt install speedtest-cli
+sudo apt install sl
+sudo apt install tint
+
+# Install zsh with oh-my-zsh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Clone dircolors theme
+curl https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark --output ~/.dircolors
+
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Clone zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
